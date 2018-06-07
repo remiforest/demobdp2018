eb0890fc304e212dceedb8f74dc3f4f91ce97339
root <root@ip-172-31-40-168.eu-west-3.compute.internal>
2018-06-07 09:42:57 +0000

    added init outputs

--- init.py
+++ init.py
@@ -6,34 +6,43 @@ import os
 with open('/opt/mapr/conf/mapr-clusters.conf', 'r') as f:
     first_line = f.readline()
     cluster_name = first_line.split(' ')[0]
-    logging.debug('Cluster name : {}'.format(cluster_name))
 
 command_line = "sudo rm -rf /mapr/" + cluster_name + "/tables"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo mkdir /mapr/" + cluster_name + "/tables"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo maprcli table create -path /tables/cargkm -tabletype json"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo maprcli table cf edit -path /tables/cargkm -cfname default readperm p -writeperm p -traverseperm p"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo mapr importJSON -src /mapr/" + cluster_name + "/demobdp2018/cargkm.json -dst /mapr/" + cluster_name + "/tables/cargkm -mapreduce false"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo maprcli table create -path /tables/count -tabletype json"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo rm -rf /mapr/" + cluster_name + "/streams"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo mkdir /mapr/" + cluster_name + "/streams"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo rm -rf /mapr/" + cluster_name + "/countries"
+print(command_line)
 os.system(command_line)
 
 command_line = "sudo mkdir /mapr/" + cluster_name + "/countries"
+print(command_line)
 os.system(command_line)
